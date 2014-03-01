da9b3d57b8316c77900242f3ff224f0285121fb3
Arshad <arshad@chummun.com>
2014-03-01 22:57:02 +0400

    Added comments

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ var http = require('http');
 var fs = require('fs');
 
 var version = require('./package.json').version;
-var url = 'https://raw.github.com/github/choosealicense.com/gh-pages/licenses/';
-
-program
-  .version(version)
-  .option('-l, --license', 'License type. Example: bsd, mit, gpl-v2..etc')
 
 program
   .command('install [license]')
   .description('Installs a license')
+  .version(version)
   .option("-y, --year <year>", 'The year to use. Example: 2014.')
+  .option("-n, --fullname <fullname>", 'Your fullname.')
   .action(function(license, options){
+    // Use provided year or default to current year.
     var year = options.year || new Date().getUTCFullYear();
-    var file = fs.createWriteStream("LICENSE");
-    var request = http.get(url + license + ".txt", function(response) {
-      response.pipe(file);
-    });
+
+    // Create a LICENSE file.
+    var file = fs.createWriteStream('LICENSE');
+
+    // Copy appropriate license.
+    fs.createReadStream('./licenses/' + license + '.txt').pipe(file);
   });
 
 program.parse(process.argv);
